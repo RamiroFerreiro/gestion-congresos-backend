9cd6f92085d52dee98d460a410f659aa88b8153b
RamiroFerreiro <ramiro.x.ferreiro@gmail.com>
2026-07-06 10:29:46 -0300

    Configuración inicial del proyecto Spring Boot

---
+++ src/main/java/com/tfi/gestion_congresos_backend/GestionCongresosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tfi.gestion_congresos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCongresosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCongresosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfi/gestion_congresos_backend/GestionCongresosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfi.gestion_congresos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCongresosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
